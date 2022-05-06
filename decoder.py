84732c14ca46d0f8b3064a8f1de920dc67ed5a38
AHMED MUSTAFA <91986681+Ahmedmustafa900@users.noreply.github.com>
2022-05-06 11:35:40 +0200

    Seminar 4
    Seminar 4

--- decoder.py
+++ decoder.py
@@ -1,41 +1,57 @@
+import numpy as np        
 import cv2
-import numpy as np
-import pickle
-import scipy.signal as sp
-import os
-import functions
+import sys
+import cPickle as pickle
+import scipy.signal
+
+f=open('videorecord.txt', 'r')
+
+rows,cols = 480,640
+n=0
+N = 2
+frame = np.zeros((480,640,3))
+frame1 = np.zeros((480,640,3))
+sampledCb = np.zeros((rows,cols))           #subsample Matrix
+sampledCr = np.zeros((rows,cols))
+
+filt1=np.ones((N,N))/N;
+filt2=scipy.signal.convolve2d(filt1,filt1)/N
+
+videolist=np.load(f)
+
+fftMr=np.ones((rows,1))                               # FFT matrix
+fftMr[int(rows/8.0):int(rows-rows/8.0),0]=np.zeros(int(3.0/4.0*rows))
+fftMc=np.ones((1,cols)) 
+fftMc[0,int(cols/8.0):int(cols-cols/8.0)]=np.zeros(int(3.0/4.0*cols));
+fftM=np.dot(fftMr,fftMc)
+
+while(True):
+
+    Y,Cb,Cr = videolist[n*3],videolist[n*3+1],videolist[n*3+2]   #convert YCbCr to rgb
+    n = n+1;
+    sampledCb[0::N,0::N] = Cb
+    sampledCr[0::N,0::N] = Cr
+    Cb=scipy.signal.convolve2d(sampledCb,filt1,mode='same')
+    Cr=scipy.signal.convolve2d(sampledCr,filt1,mode='same')
 
 
-dctt = open('dct.txt', 'rb')
-xf = open('full.txt', 'rb')
+    #cv2.imshow('Y',Y)
+    #cv2.imshow('cb',Cb)
+    #cv2.imshow('cr',Cr)
 
-try:
-    while True:
+    Y = Y*255
+    Cb = Cb*255-128
+    Cr = Cr*255-128
 
-        dctReduced = pickle.load(dctt)
-        fullFrame = pickle.load(xf)
+    r = (Y+Cr*1.4025)
+    g = (Y+Cb*(-0.34434)+Cr*(-0.7144))
+    b = (Y+Cb*1.7731)
 
-        dct = functions.DCTWithZeros(dctReduced, functions.DTCFactor)
+    frame[:,:,0],frame[:,:,1],frame[:,:,2] = b,g,r
 
-        idctFrame = functions.IDCTFrame(dct)
+    cv2.imshow('Video',frame/255)
 
-        frame_sub = functions.chomaSubSamp(idctFrame[:,:,0], idctFrame[:,:,1], idctFrame[:,:,2])
+    if cv2.waitKey(40) & 0xFF == ord('q'):
+        break
 
-
-        if functions.filterON:
-            frame_filt = functions.filterFrame(frame_sub, functions.desiredFilter)
-        else:
-            frame_filt = frame_sub
-        RGB_frame = functions.Ycbcr2Rgb(frame_filt)
-
-        cv2.imshow('RGB frame', RGB_frame)
-        if cv2.waitKey(200) == ord('q'):
-            break
-
-except (EOFError):
-    pass
-
-dctt.close()
-xf.close()
 cv2.destroyAllWindows()
-
--- encoder.py
+++ encoder.py
@@ -1,91 +1,78 @@
-import cv2
 import numpy as np
-import pickle
-import scipy.signal as sp
-import os
-import functions
-
-f_downsampled = open('subs.txt', 'wb')
-f_full = open('full.txt', 'wb')
-encDCT = open('dct.txt', 'wb')
+import cv2
+import pickle 
+import scipy.signal
 
 cap = cv2.VideoCapture(0)
 
+f=open('videorecord.txt', 'wb')
+
+framenum = 200
+N = 2      #subsample value
+data = []
+
+[ret, frame] = cap.read()
+[rows,cols,c]=frame.shape;
+
+filt1=np.ones((N,N))/N;
+filt2=scipy.signal.fftconvolve(filt1,filt1)/N
+
+Yprev=np.zeros((rows,cols))
+framevectors=np.zeros((rows,cols,3))
+mv=np.zeros((int(rows/8),int(cols/8),2))
+
+for n in range(framenum):
 #while(True):
-for i in range(25):
-    ret, frame = cap.read()
-    reduced = frame.copy()
+    
+    print("Frame no",n)
+    [ret, frame] = cap.read()
 
-    if functions.filterON:
-        frameFilt = functions.filterFrame(functions.Rgb2Ycbcr(reduced), functions.desiredFilter)
+    if ret==True:
+
+        b,g,r = frame[:,:,0],frame[:,:,1],frame[:,:,2]   #convert rgb to YCbCr
+
+        Y = (r*0.299+g*0.587+b*0.114)/255;   #0~255
+        Cb = ((r*(-0.16864)+g*(-0.33107)+b*0.4997)+128)/255;  #0~255
+        Cr = ((r*0.499813+g*(-0.418531)+b*(-0.081282))+128)/255;  #0~255
+
+
+        cv2.imshow('Original',frame/255.0+framevectors)
+
+        filtCr=scipy.signal.fftconvolve(Cr,filt2,mode='same')
+        filtCb=scipy.signal.fftconvolve(Cb,filt2,mode='same')
+
+        sampledCb = filtCb[0::N,0::N]      #4:2:0
+        sampledCr = filtCr[0::N,0::N]
+
+        block=np.array([8,8])
+        framevectors=np.zeros((rows,cols,3))
+       
+        for yblock in range(30):
+           block[0]=yblock*8+200;
+           for xblock in range(40):
+              block[1]=xblock*8+300;
+              Yc=Y[block[0]:block[0]+8 ,block[1]:block[1]+8]   #current block
+	      Yp=Yprev[block[0]-4 : block[0]+12 ,block[1]-4 : block[1]+12]  #previous block
+              Ycorr=scipy.signal.correlate2d(Yp, Yc,mode='valid')
+              index1d=np.argmax(Ycorr)
+              index2d=np.unravel_index(index1d,(9,9))
+              mv[yblock,xblock]=np.subtract(index2d,(4,4))  
+              if sum(mv[yblock,xblock]) != 0:              
+                 cv2.line(framevectors, (block[1], block[0]), (block[1]+mv[yblock,yblock,1].astype(int),block[0]+mv[yblock,yblock,0].astype(int)) , (1.0,1.0,1.0));
+        Yprev=Y.copy();
+
+        data.append(Y)
+        data.append(sampledCb)
+        data.append(sampledCr)
+
+        key=cv2.waitKey(1) & 0xFF;
+        if key == ord('q'):
+           break
     else:
-        frameFilt = functions.Rgb2Ycbcr(reduced)
-
-    Y, Cb, Cr = frameFilt[:, :, 0], frameFilt[:, :, 1], frameFilt[:, :, 2]
-    cv2.imshow('Y Frame', Y/255.0)
-    #Chroma Downsampling with zeros
-
-    Cb_downsampled = np.zeros(Cb.shape)
-    Cb_downsampled[0::2, 0::2] = Cb[0::2, 0::2]
-    Cr_downsampled = np.zeros(Cr.shape)
-    Cr_downsampled[0::2, 0::2] = Cr[0::2, 0::2]
-
-    #cv2.imshow('DCb Frame', Cb_downsampled / 255.0)
-    #cv2.imshow('DCr Frame', Cr_downsampled / 255.0)
-    #cv2.imshow('Original', reduced)
-    
-    frame_subsample = np.zeros((reduced.shape))
-    frame_subsample[:,:,0] = Y
-    frame_subsample[:,:,1] = Cb_downsampled
-    frame_subsample[:,:,2] = Cr_downsampled
-    
-    ssycbcr_wo_zeros = np.zeros((240,320,2)) 
-    ssycbcr_wo_zeros[0::, 0::, 0]=frame_subsample[0::2, 0::2, 1]
-    ssycbcr_wo_zeros[0::, 0::, 1]=frame_subsample[0::2, 0::2, 2]
-        	
-    ssycbcr_wo_zeros = functions.DCTRemoveZeros(frame_subsample, functions.DTCFactor)
-        
-    #frame_subsample = np.zeros((reduced.shape))
-    #frame_subsample[:,:,0] = Y
-    #frame_subsample[:,:,1] = Cb_downsampled
-    #frame_subsample[:,:,2] = Cr_downsampled 
-
-    dcty = functions.applyDCT(Y)
-    dctcb = functions.applyDCT(ssycbcr_wo_zeros[::, ::, 0])
-    dctcr = functions.applyDCT(ssycbcr_wo_zeros[::, ::, 1])
-    
-    cv2.imshow('Y DCT', np.abs(dcty[:,:,0]))
-    cv2.imshow('Cr DCT', np.abs(dctcb[:, :, 1]))
-    cv2.imshow('Cb DCT', np.abs(dctcr[:, :, 2]))
-
-    #dct_wo_Zeros = functions.DCTRemoveZeros(dct, functions.DTCFactor)
-    
-    enc = np.zeros(frame.shape, np.int8)
-    enc[:, :, 0] = Y
-    enc[:, :, 1] = Cr_downsampled
-    enc[:, :, 2] = Cb_downsampled
-
-    # Storing the data    
-    pickle.dump(enc, f_full)
-
-    # Zero removed downsampled version
-    pickle.dump(Y, f_downsampled)
-    pickle.dump(ssycbcr_wo_zeros[::, ::, 0], f_downsampled)
-    pickle.dump(ssycbcr_wo_zeros[::, ::, 1], f_downsampled)
-
-    # Storing DCT data
-    #pickle.dump(dct_wo_Zeros.astype(np.float16), encDCT)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-print("Size of encoded DCT data: MB", format(encDCT.tell()/1024**2.))
-print("original file:   MB",format(f_full.tell()/1024**2.))
-print("compression ratio", (f_full.tell())/encDCT.tell())
+np.save(f,data)
 
-
-f_downsampled.close()
-f_full.close()
-encDCT.close()
 cap.release()
+f.close()
 cv2.destroyAllWindows()
